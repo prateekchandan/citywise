5179c931a886113568de4937282a845d7c723d5c
prateekchandan <prateekchandan5545@gmail.com>
2014-11-11 00:36:36 +0530

    database connection

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'citywise',
+			'username'  => 'neha',
+			'password'  => 'nehachandan',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,9 @@
 
 Route::get('/', function()
 {
+	if(DB::connection()->getDatabaseName())
+		{
+		   echo "conncted sucessfully to database ".DB::connection()->getDatabaseName();
+		}
 	return View::make('hello');
 });
